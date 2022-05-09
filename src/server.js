044d55f6fb60f5e1b03001a3bf93cd4046cd6c67
blvckeasy <abdurakhmonovislom9@gmail.com>
2022-05-09 23:49:25 +0600

    index.js deleted :)

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
+import { createServer } from "http"
+import { Server } from "socket.io"
+import express from 'express'
+import path from "path"
+
 import paginationMiddleware from './middlewares/pagination.js'
 import modelMiddleware from './middlewares/model.js'
 import tableRouter from './routes/table.js'
@@ -5,9 +10,10 @@ import mainRouter from './routes/main.js'
 import userRouter from './routes/user.js'
 import authRouter from './routes/auth.js'
 import { PORT, host } from '../config.js'
-import express from 'express'
+
 
 const app = express()
+const httpServer = createServer(app)
 
 app.use(express.json())
 app.use(express.static('upload'))
@@ -18,8 +24,19 @@ app.use('/api/auth/', authRouter)
 app.use('/api/users/', userRouter)
 app.use('/api/mainpage/', mainRouter)
 
+const io = new Server(httpServer,{
+  cors: {
+    origin: "http://192.168.1.107:3000"
+  }
+})
 
 
-app.listen(PORT, () =>
+io.on("connection", (socket) => {
+  console.log(socket)
+})
+
+
+
+httpServer.listen(PORT, () =>
   console.log(`server is listening on http://${host}:${PORT}/api/`)
 )
